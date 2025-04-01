2b3e5202dfceeb410dfadf140bc336e840b37d18
nicolas perrault <nipe1783@colorado.edu>
2025-04-01 12:24:46 -0600

    hw5 done

--- hw/5-Deep-Reinforcement-Learning/q3.jl
+++ hw/5-Deep-Reinforcement-Learning/q3.jl
@@ -7,64 +7,113 @@ using QuickPOMDPs: QuickPOMDP
 using POMDPTools: Deterministic, Uniform, SparseCat, FunctionPolicy, RolloutSimulator
 using Statistics: mean
 import POMDPs
+using Plots
 using CommonRLInterface
 using Flux
 using CommonRLInterface.Wrappers: QuickWrapper
 
-# The following are some basic components needed for DQN
 
-# Override to a discrete action space, and position and velocity observations rather than the matrix.
+function epsilon_greedy_action(env, Q, s, eps)
+    if rand() < eps
+        return rand(1:length(actions(env)))
+    else
+        return argmax(Q(s))
+    end
+end
+
+function loss(Q, Q_target, s, a_ind, r, sp, terminal; gamma=0.99)
+    if terminal
+        return (r - Q(s)[a_ind])^2
+    end
+    return (r + gamma * maximum(Q_target(sp)[ap_ind] for ap_ind in 1:length(actions(env))) - Q(s)[a_ind])^2
+end
+
+function dqn(env; maxBufferSize=100000, batchSize=32, updateFrequency=1000)
+
+    Q = Chain(Dense(2, 128, relu),
+        Dense(128, length(actions(env))))
+    Q_target = deepcopy(Q)
+    opt = Flux.setup(ADAM(0.0005), Q)
+
+    theBuffer = []
+    lossexperience = Float32[] # for plotting
+    rewardexperience = Float32[] # for plotting
+
+    # Initialize state, terminal flag, and number of steps
+    s = observe(env)
+    terminal = false
+    stepNum = 0
+    training = true
+    maxSteps = 150000
+
+    while training && stepNum < maxSteps
+
+        # take epsilon greedy action, build experience, store in theBuffer
+        eps = max(0.05, 0.5 - stepNum / maxBufferSize)
+        a_ind = epsilon_greedy_action(env, Q, s, eps)
+        r = act!(env, actions(env)[a_ind])
+        sp = observe(env)
+        terminal = terminated(env)
+        stepNum += 1
+        experience_tuple = (s, a_ind, r, sp, terminal)
+        push!(theBuffer, experience_tuple)
+
+        # remove oldest experience from buffer
+        if length(theBuffer) > maxBufferSize
+            popfirst!(theBuffer)
+        end
+
+        # Sample a batch of experiences from theBuffer
+        avgLoss = 0.0
+        for experience in rand(theBuffer, batchSize)
+            lossVal, grads = Flux.withgradient(loss, Q, Q_target, experience...)
+            Flux.update!(opt, Q, grads[1])
+            avgLoss += lossVal
+        end
+
+        if terminal
+            reset!(env)
+        else
+            s = sp
+        end
+
+        # Update the target network every updateFrequency steps
+        if stepNum % updateFrequency == 0
+            Q_target = deepcopy(Q)
+        end
+
+        # Log the average loss and reward every 1000 steps
+        avgReward = 0.0
+        if stepNum % 1000 == 0
+            eval = HW5.evaluate(s -> actions(env)[argmax(Q(s[1:2]))], n_episodes=1000)
+            avgReward = eval[:score]
+            println("Steps: $stepNum, Avg Reward: $avgReward")
+            push!(lossexperience, avgLoss / batchSize)
+            push!(rewardexperience, avgReward)
+        end
+
+        # Check if training should stop
+        if avgReward >= 40.0
+            eval = HW5.evaluate(s -> actions(env)[argmax(Q(s[1:2]))], "nicolas.perrault@colorado.edu", n_episodes=10000)
+            if eval[:score] >= 40.0
+                training = false
+            end
+        end
+    end
+
+    return Q, lossexperience, rewardexperience
+end
+
 env = QuickWrapper(HW5.mc,
     actions=[-1.0, -0.5, 0.0, 0.5, 1.0],
     observe=mc -> observe(mc)[1:2]
 )
 
-function dqn(env)
-    # This network should work for the Q function - an input is a state; the output is a vector containing the Q-values for each action 
-    Q = Chain(Dense(2, 128, relu),
-        Dense(128, length(actions(env))))
-
-    opt = Flux.setup(ADAM(0.0005), Q)
-
-
-    # We can create 1 tuple of experience like this
-    s = observe(env)
-    a_ind = 1 # action index - the index, rather than the actual action itself, will be needed in the loss function
-    r = act!(env, actions(env)[a_ind])
-    sp = observe(env)
-    done = terminated(env)
-
-    experience_tuple = (s, a_ind, r, sp, done)
-
-    # this container should work well for the experience buffer:
-    buffer = [experience_tuple]
-    # you will need to push more experience into it and randomly select data for training
-
-    reset!(env) # NOTE: after each time the environment reaches a terminal state, you need to reset it
-
-    # this is the fixed target Q network
-    Q_target = deepcopy(Q)
-
-    # create your loss function for Q training here
-    function loss(Q, s, a_ind, r, sp, done)
-        return (Q(s)[a_ind] - Q_target(s)[a_ind])^2 # this is not correct! you need to replace it with the true Q-learning loss function
-        # make sure to take care of cases when the problem has terminated correctly
-    end
-
-    # select some data from the buffer
-    data = rand(buffer, 10)
-
-    # do your training like this (you may have to adjust some things, and you will have to do this many times):
-    Flux.Optimise.train!(loss, Q, data, opt)
-
-    # Make sure to evaluate, print, and plot often! You will want to save your best policy.
-
-    return Q
-end
-
-Q = dqn(env)
-
-# HW5.evaluate(s -> actions(env)[argmax(Q(s[1:2]))], n_episodes=100) # you will need to remove the n_episodes=100 keyword argument to create a json file; evaluate needs to run 10_000 episodes to produce a json
+result = dqn(env)
+Q = result[1] # Extract the trained Q network from the result
+loss_curve = result[2] # Extract the loss curve for plotting
+reward_curve = result[3] # Extract the reward curve for plotting
+number_steps = length(loss_curve)
 
 #----------
 # Rendering
@@ -74,23 +123,16 @@ Q = dqn(env)
 display(render(env))
 
 # The following code allows you to render the value function
-# using Plots
-# xs = -3.0f0:0.1f0:3.0f0
-# vs = -0.3f0:0.01f0:0.3f0
-# heatmap(xs, vs, (x, v) -> maximum(Q([x, v])), xlabel="Position (x)", ylabel="Velocity (v)", title="Max Q Value")
+xs = -3.0f0:0.1f0:3.0f0
+vs = -0.3f0:0.01f0:0.3f0
+heatmap(xs, vs, (x, v) -> maximum(Q([x, v])), xlabel="Position (x)", ylabel="Velocity (v)", title="Max Q Value")
+savefig("DQN_ValueFunc.png")
 
+# Plot the loss and reward curves over training iterations
+p1 = plot(loss_curve; label="Loss Vs Iterations", xlabel="Training Steps (x 1000)", ylabel="Loss", title="Loss Vs Iterations", yaxis=:log)
+plot(p1; legend=:topright)
+savefig("DQN_loss_curve.png") # Save the plot to a file
 
-# function render_value(value)
-#     xs = -3.0:0.1:3.0
-#     vs = -0.3:0.01:0.3
-# 
-#     data = DataFrame(
-#                      x = vec([x for x in xs, v in vs]),
-#                      v = vec([v for x in xs, v in vs]),
-#                      val = vec([value([x, v]) for x in xs, v in vs])
-#     )
-# 
-#     data |> @vlplot(:rect, "x:o", "v:o", color=:val, width="container", height="container")
-# end
-# 
-# display(render_value(s->maximum(Q(s))))
+p2 = plot(reward_curve; label="Reward Curve", xlabel="Training Steps (x 1000)", ylabel="Average Reward", title="Average Reward Vs Iterations") # Plot the reward curve 
+plot(p2; legend=:topright)
+savefig("DQN_reward_curve.png") # Save the plot to a file
